8f936ee116ae654b043edd0bf4427251430625fc
Wolfgang Zdrenka <wzdrenka@gmail.com>
2021-06-16 15:57:56 +1200

    Inityial commit.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { Geolocation, Position } from '@capacitor/geolocation';
+import { Share } from '@capacitor/share';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,36 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  myImage = null;
+  position: Position = null;
+
   constructor() {}
+
+  async takePicture() {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: true,
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera
+    });
+
+    this.myImage = image.webPath;
+  }
+
+  async getCurrentPosition() {
+    const coordinates = await Geolocation.getCurrentPosition();
+
+    this.position = coordinates;
+  }
+
+  async share() {
+    await Share.share({
+      title: 'Come and find me',
+      text: `Here's my current location: 
+        ${this.position.coords.latitude}, 
+        ${this.position.coords.longitude}`,
+      url: 'http://ionicacademy.com/'
+    });
+  }
 
 }
